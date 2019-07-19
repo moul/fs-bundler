79d4e05f22030f853dd2a0219c0683e8ea3cdbd1
Manfred Touron <m@42.am>
2019-07-19 22:00:33 +0200

    chore: initial version

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module moul.io/fs-bundler
 
 go 1.12
+
+require (
+	github.com/mattn/go-zglob v0.0.1
+	github.com/pkg/errors v0.8.1
+	gopkg.in/urfave/cli.v2 v2.0.0-20180128182452-d3ae77c26ac8
+)
--- main.go
+++ main.go
@@ -1,7 +1,78 @@
 package main // import "moul.io/fs-bundler"
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"sort"
+
+	zglob "github.com/mattn/go-zglob"
+	"github.com/pkg/errors"
+	cli "gopkg.in/urfave/cli.v2"
+)
+
+type Dump struct {
+	Files []File `json:"files"`
+}
+
+type File struct {
+	Path    string `json:"path"`
+	Name    string `json:"name"`
+	Content []byte `json:"content"`
+	// Type string `json:"type"` // mime
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	app := &cli.App{
+		Name:  "fs-bundler",
+		Flags: []cli.Flag{
+			// compress
+			// output (yml, json, ...)
+		},
+		Action: func(c *cli.Context) error {
+			targets := c.Args().Slice()
+			if len(targets) == 0 {
+				targets = append(targets, ".")
+			}
+			allMap := map[string]bool{}
+			for _, target := range targets {
+				matches, err := zglob.Glob(target)
+				if err != nil {
+					return errors.Wrapf(err, "failed to list files from %q", target)
+				}
+				for _, match := range matches {
+					allMap[match] = true
+				}
+			}
+			all := []string{}
+			for fullPath := range allMap {
+				all = append(all, fullPath)
+			}
+			sort.Strings(all)
+			dump := Dump{}
+			for _, fullPath := range all {
+				content, err := ioutil.ReadFile(fullPath)
+				if err != nil {
+					log.Printf("failed to read content from %q: %v", fullPath, err)
+					continue
+				}
+				dump.Files = append(dump.Files, File{
+					Path:    fullPath,
+					Name:    path.Base(fullPath),
+					Content: content,
+				})
+			}
+			out, err := json.MarshalIndent(dump, "", "  ")
+			if err != nil {
+				return err
+			}
+			fmt.Println(string(out))
+			return nil
+		},
+	}
+
+	app.Run(os.Args)
 }
